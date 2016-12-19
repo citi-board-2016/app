3c15625ace0777606dec689a10b875bb5013c2b9
Alison Lee <lee.alison@nyu.edu>
2016-12-19 15:49:36 -0500

    Updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var all_routes_1 = [];
+var all_routes_2 = [];
+var all_routes_3 = [];
+var all_routes_4 = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'citiboard' });
+});
+
+router.get('/stations', function(req, res) {
+  if (req.query.id == 1)
+    res.render('stations', {heading: 'Starting station', station: '1 Ave & E 16 St', numRoutes: '13'});
+  else if (req.query.id == 2)
+    res.render('stations', {heading: 'Starting station', station: '10 St & 5 Ave', numRoutes: '12'});
+  else if (req.query.id == 3)
+    res.render('stations', {heading: 'Ending station', station: '6 Ave & Broome St', numRoutes: '146'});
+  else if (req.query.id == 4)
+    res.render('stations', {heading: 'Ending station', station: 'Division St & Bowery', numRoutes: '115'});
+});
+
+router.get('/boards', function(req, res) {
+  res.render('boards');
 });
 
 module.exports = router;
